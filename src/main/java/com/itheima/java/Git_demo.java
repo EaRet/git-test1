c0f84ce1ca093724ebdfe817e0bfeb9513170dcb
zhang <zh000L00@outlook.com>
2022-12-12 21:18:12 +0800

    hot-fix first commit

--- src/main/java/com/itheima/java/Git_demo.java
+++ src/main/java/com/itheima/java/Git_demo.java
@@ -2,8 +2,11 @@ package com.itheima.java;
 
 public class Git_demo {
     public static void main(String[] args) {
-        System.out.println("hello,git");
-        System.out.println("hello,git");
+        System.out.println("hello,git1");
+        System.out.println("hello,git2");
+        System.out.println("hello,git3");
+        System.out.println("hello,git4");
+        System.out.println("hello,git5");
         System.out.println("hello,git");
     }
 }
