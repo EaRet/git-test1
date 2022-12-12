a9a5173c69ef93b12f832c2dd00c7d5d953ffacc
zhang <zh000L00@outlook.com>
2022-12-12 21:16:25 +0800

    master first commit

---
+++ src/main/java/com/itheima/java/Git_demo.java
@@ -0,0 +1,9 @@
+package com.itheima.java;
+
+public class Git_demo {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+    }
+}
